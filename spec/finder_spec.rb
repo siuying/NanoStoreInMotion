ee5b4f77e710e8a29d34498a0da09b5b835ea673
Francis Chong <francis@ignition.hk>
2012-12-16 10:27:24 +0800

    rewrite finder spec make it cleaner

--- spec/finder_spec.rb
+++ spec/finder_spec.rb
@@ -31,156 +31,162 @@ describe "Finder" do
     user3.save
   end
 
-  it "create object in their class" do
-    NanoStore.shared_store.allObjectClasses.should == ["User"]
-    Car.create(:name => "Bob")
-    NanoStore.shared_store.allObjectClasses.sort.should == ["User", "Car"].sort
+  describe "::create" do
+    it "create object in their class" do
+      NanoStore.shared_store.allObjectClasses.should == ["User"]
+      Car.create(:name => "Bob")
+      NanoStore.shared_store.allObjectClasses.sort.should == ["User", "Car"].sort
+    end
   end
 
-  it "search object traditional way: supply key, operator and value" do
-    users = User.find(:name, NSFEqualTo, "Bob")
-    users.should.not.be.nil
-    user = users.first
-    user.should.not.be.nil
-    user.name.should.be == "Bob"
-    user.age.should.be == 10
+  describe "::find" do
+    it "search object traditional way: supply key, operator and value" do
+      users = User.find(:name, NSFEqualTo, "Bob")
+      users.should.not.be.nil
+      user = users.first
+      user.should.not.be.nil
+      user.name.should.be == "Bob"
+      user.age.should.be == 10
+    end
+
+    it "search object with simple hash" do
+      user = User.find(:name => "Carl").first
+      user.name.should.be == "Carl"
+      user.age.should.be == 4
+    end
+
+    it "search object with simple hash with two class" do
+      Car.create(:name => "Carl")
+      users = User.find(:name => "Carl")
+      users.size.should.be == 1
+      user = users.first
+      user.name.should.be == "Carl"
+      user.age.should.be == 4
+    end
+
+    it "search object with array (OR)" do
+      users = User.find(:name => ["Carl", "Amy"])
+      users.size.should == 2
+
+      users.collect(&:name).include?("Carl").should == true
+      users.collect(&:name).include?("Amy").should == true
+      users.collect(&:name).include?("Bob").should == false
+    end
+
+    it "search object with multiple parameters" do
+      stub_user("Ronald", 18, Time.now).save
+      stub_user("Ronald", 29, Time.now).save
+      stub_user("Ronald", 36, Time.now).save
+      stub_user("Ronald", 39, Time.now).save
+      stub_user("Ronald", 49, Time.now).save
+      stub_user("Ronald", 59, Time.now).save
+
+      users = User.find(:name => { NSFEqualTo => "Ronald" }, :age => { NSFGreaterThan => 50 })
+      users.size.should == 1
+      user = users.first
+      user.name.should.be == "Ronald"
+      user.age.should.be == 59
+
+      users = User.find(:name => { NSFEqualTo => "Ronald" }, :age => { NSFLessThan => 30 })
+      users.size.should == 2
+      user = users.first
+      user.name.should.be == "Ronald"
+    end
+
+    it "sort search results" do
+      stub_user("Alan", 39, Time.now).save
+      stub_user("Cat", 29, Time.now).save
+      stub_user("Dan", 36, Time.now).save
+      stub_user("Ted", 18, Time.now).save
+      stub_user("Zidd", 59, Time.now).save
+      stub_user("Sarah", 49, Time.now).save
+
+      users = User.find({:age => { NSFGreaterThan => 1 }}, {:sort => {:age => :asc}})
+      users.size.should == 9
+      user = users.first
+      user.name.should.be == "Carl"
+      user.age.should.be == 4
+      user = users.last
+      user.name.should.be == "Zidd"
+      user.age.should.be == 59
+
+      users = User.find({:age => { NSFGreaterThan => 1 }}, {:sort => {:age => 'DESC'}})
+      users.size.should == 9
+      user = users.last
+      user.name.should.be == "Carl"
+      user.age.should.be == 4
+      user = users.first
+      user.name.should.be == "Zidd"
+      user.age.should.be == 59
+    end
+
+    it "find object" do
+      users = User.find(:name, NSFEqualTo, "Bob")
+      users.size.should == 1
+
+      user = users.first
+      user.name.should == "Bob"
+      user.class.should == User
+    end
+
+    it "only return objects of the class" do
+      car = Car.create(:name => "Honda")
+      Car.find.size.should == 1
+      Car.find.first.key.should == car.key
+    end
   end
 
-  it "search object with simple hash" do
-    user = User.find(:name => "Carl").first
-    user.name.should.be == "Carl"
-    user.age.should.be == 4
+  describe "::find_by_key" do
+    it "should return object by key" do
+      name = "Julien"
+      age = 32
+      created_at = Time.now
+      user = stub_user("Julien", 32, Time.now)
+      user.save
+      user2 = User.find_by_key(user.key)
+      user2.should.not.be.nil
+      user2.key.should == user.key
+      user2.name.should == user.name
+      user2.age.should == user.age
+    end
+
+    it "should not find object by inexists key" do
+      User.find_by_key("invalid-key").should.be.nil
+    end
   end
 
-  it "search object with simple hash with two class" do
-    Car.create(:name => "Carl")
-    users = User.find(:name => "Carl")
-    users.size.should.be == 1
-    user = users.first
-    user.name.should.be == "Carl"
-    user.age.should.be == 4
+  describe "::all" do
+    it "find all objects" do
+      User.count.should == 3
+      users = User.all
+      users.size.should == 3
+    end
+
+    it "find all objects, sorted" do
+      stub_user("Alan", 39, Time.now).save
+      stub_user("Cat", 29, Time.now).save
+      stub_user("Dan", 36, Time.now).save
+      stub_user("Ted", 18, Time.now).save
+      stub_user("Zidd", 59, Time.now).save
+      stub_user("Sarah", 49, Time.now).save
+
+      User.count.should == 9
+      users = User.all({:sort => {:age => :asc}})
+      users.size.should == 9
+      users.first.name.should == "Carl"
+    end
+
+    it "only return objects of the class" do
+      car = Car.create(:name => "Honda")
+      Car.count.should == 1
+      Car.all.size.should == 1
+      Car.all.first.key.should == car.key
+    end
+
+    it "only return objects of the class" do
+      car = Car.create(:name => "Honda")
+      Car.find_keys.size.should == 1
+      Car.find_keys.first.should == car.key
+    end
   end
-
-  it "search object with array (OR)" do
-    users = User.find(:name => ["Carl", "Amy"])
-    users.size.should == 2
-
-    users.collect(&:name).include?("Carl").should == true
-    users.collect(&:name).include?("Amy").should == true
-    users.collect(&:name).include?("Bob").should == false
-  end
-
-
-  it "search object with multiple parameters" do
-    stub_user("Ronald", 18, Time.now).save
-    stub_user("Ronald", 29, Time.now).save
-    stub_user("Ronald", 36, Time.now).save
-    stub_user("Ronald", 39, Time.now).save
-    stub_user("Ronald", 49, Time.now).save
-    stub_user("Ronald", 59, Time.now).save
-
-    users = User.find(:name => { NSFEqualTo => "Ronald" }, :age => { NSFGreaterThan => 50 })
-    users.size.should == 1
-    user = users.first
-    user.name.should.be == "Ronald"
-    user.age.should.be == 59
-
-    users = User.find(:name => { NSFEqualTo => "Ronald" }, :age => { NSFLessThan => 30 })
-    users.size.should == 2
-    user = users.first
-    user.name.should.be == "Ronald"
-  end
-
-  it "sort search results" do
-    stub_user("Alan", 39, Time.now).save
-    stub_user("Cat", 29, Time.now).save
-    stub_user("Dan", 36, Time.now).save
-    stub_user("Ted", 18, Time.now).save
-    stub_user("Zidd", 59, Time.now).save
-    stub_user("Sarah", 49, Time.now).save
-
-    users = User.find({:age => { NSFGreaterThan => 1 }}, {:sort => {:age => :asc}})
-    users.size.should == 9
-    user = users.first
-    user.name.should.be == "Carl"
-    user.age.should.be == 4
-    user = users.last
-    user.name.should.be == "Zidd"
-    user.age.should.be == 59
-
-    users = User.find({:age => { NSFGreaterThan => 1 }}, {:sort => {:age => 'DESC'}})
-    users.size.should == 9
-    user = users.last
-    user.name.should.be == "Carl"
-    user.age.should.be == 4
-    user = users.first
-    user.name.should.be == "Zidd"
-    user.age.should.be == 59
-  end
-
-  it "find object" do
-    users = User.find(:name, NSFEqualTo, "Bob")
-    users.size.should == 1
-
-    user = users.first
-    user.name.should == "Bob"
-    user.class.should == User
-  end
-
-  it "find object by key" do
-    name = "Julien"
-    age = 32
-    created_at = Time.now
-    user = stub_user("Julien", 32, Time.now)
-    user.save
-    user2 = User.find_by_key(user.key)
-    user2.should.not.be.nil
-    user2.key.should == user.key
-    user2.name.should == user.name
-    user2.age.should == user.age
-  end
-
-  it "should not find object by key" do
-    User.find_by_key("invalid-key").should.be.nil
-  end
-
-  it "find all objects" do
-    User.count.should == 3
-    users = User.all
-    users.size.should == 3
-  end
-
-  it "find all objects, sorted" do
-    stub_user("Alan", 39, Time.now).save
-    stub_user("Cat", 29, Time.now).save
-    stub_user("Dan", 36, Time.now).save
-    stub_user("Ted", 18, Time.now).save
-    stub_user("Zidd", 59, Time.now).save
-    stub_user("Sarah", 49, Time.now).save
-
-    User.count.should == 9
-    users = User.all({:sort => {:age => :asc}})
-    users.size.should == 9
-    users.first.name.should == "Carl"
-  end
-
-  it "#all only return objects of the class" do
-    car = Car.create(:name => "Honda")
-    Car.count.should == 1
-    Car.all.size.should == 1
-    Car.all.first.key.should == car.key
-  end
-
-  it "#find only return objects of the class" do
-    car = Car.create(:name => "Honda")
-    Car.find.size.should == 1
-    Car.find.first.key.should == car.key
-  end
-
-  it "#find_keys only return objects of the class" do
-    car = Car.create(:name => "Honda")
-    Car.find_keys.size.should == 1
-    Car.find_keys.first.should == car.key
-  end
-
 end
